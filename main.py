1e265a17c711af24469a87ef2a7b51b0502dc7c6
asaHorn <horntx3@gmail.com>
2021-11-08 14:21:59 -0500

    This is going to be too easy....

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ bot = commands.Bot(command_prefix='!')
 
 @bot.command()
 async def run(ctx, *args):
-    os.system("echo hi there")
+    os.system('powershell [console]::beep(500,300)')
 
 
 f = open('secrets.txt')
