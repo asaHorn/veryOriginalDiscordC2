bcc617893e2a7e7309782d15a5895c6ed2275ce4
asaHorn <horntx3@gmail.com>
2021-11-20 12:56:02 -0500

    Working program output

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 ###
 
 ###
-import os
+import shlex
+from subprocess import Popen, PIPE
 
 import discord
 from discord.ext import commands
@@ -11,11 +12,18 @@ bot = commands.Bot(command_prefix='!')
 
 @bot.command()
 async def run(ctx, *args):
-    command = ""
-    for word in args:
-        command += word + ' '
-    os.system(command)
+    cmd = ''
+    for bit in args:
+        cmd += bit + ' '
+    print('USR CMD: ' + cmd)
+    command = shlex.split(cmd)
+    process = Popen(command, shell=True, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    stdout = stdout.decode("utf-8")
 
+    print('----returns----')
+    print(stdout)
+    await ctx.send('```' + str(stdout) + '```')
 
 f = open('secrets.txt')
 secret = f.read()
